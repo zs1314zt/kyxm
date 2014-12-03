dc91ee2c2189cd64b50c06cd8948594c9c223d31
zs1314zt <994249762@qq.com>
2014-12-03 20:30:24 +0800

    fghjf

---
+++ index.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>科研所</title>
+
+    
+    <link rel="stylesheet" href="css/styles.css" type="text/css" />
+	<link rel="stylesheet" href="css/jquery-tool.css" type="text/css" />
+
+
+    <link rel="stylesheet" type="text/css" href="jqueryeasyui/themes/default/easyui.css">
+    <link rel="stylesheet" type="text/css" href="jqueryeasyui/themes/icon.css">
+    <link rel="stylesheet" type="text/css" href="jqueryeasyui/demo.css">
+    <script type="text/javascript" src="jqueryeasyui/jquery.min.js"></script>
+    <script type="text/javascript" src="jqueryeasyui/jquery.easyui.min.js"></script>
+
+    <script type="text/javascript" src="js/jquery.tools.min.js"></script>
+	<script type="text/javascript" src="js/main.js"></script>
+
+</head>
+<body>
+    <div id="cc" class="easyui-layout" style="width:100%;height:650px;">
+        <div data-options="region:'north'" style="height:105px"><img src="images/index/logo.png"/></div>
+        <div data-options="region:'south',split:true" style="height:50px;"></div>
+        <div data-options="region:'east',split:true" title="East" style="width:100px;"></div>
+        <div data-options="region:'west',split:true" title="West" style="width:207px;">
+		<ul id="expmenu-freebie">
+			<li>
+				<!-- Start Freebie -->
+				<ul class="expmenu">
+					<li>
+						<div class="header">
+							<span class="label" style="background-image: url(images/messages.png);">我的项目</span>
+							<span class="arrow up"></span>
+						</div>
+						<ul class="left_menu">
+							<li>项目查询</li>
+							<li class="selected">项目申报</li>
+							<li>项目进度</li>
+							<li>项目经费</li>
+							<li>项目成果</li>
+						</ul>
+					</li>
+					<li>
+						<div class="header">
+							<span class="label" style="background-image: url(images/messages.png);">所有项目</span>
+							<span class="arrow up"></span>
+						</div>
+						<ul class="left_menu">
+							<li class="selected">项目查询</li>
+							<li>项目进度</li>
+							<li>项目成果</li>
+						</ul>
+					</li>
+					
+					<li>
+						<div class="header">
+							<span class="label" style="background-image: url(images/search.png);">信息修改</span>
+						</div>
+					</li>
+				</ul>
+				<!-- End Freebie -->
+			</li>
+		</ul>
+		</div><!--endwest-->
+        <div data-options="region:'center',title:'Center'">
+		
+	<table class="easyui-datagrid" title="DataGrid Complex Toolbar" style="width:100%;height:100%"
+            data-options="rownumbers:true,singleSelect:true,url:'datagrid_data1.json',method:'get',toolbar:'#tb',footer:'#ft'">
+        <thead>
+            <tr>
+                <th data-options="field:'itemid',width:80">Item ID</th>
+                <th data-options="field:'productid',width:100">Product</th>
+                <th data-options="field:'listprice',width:80,align:'right'">List Price</th>
+                <th data-options="field:'unitcost',width:80,align:'right'">Unit Cost</th>
+                <th data-options="field:'attr1',width:240">Attribute</th>
+                <th data-options="field:'status',width:60,align:'center'">Status</th>
+            </tr>
+        </thead>
+    </table>
+    <div id="tb" style="padding:2px 5px;">
+        Date From: <input class="easyui-datebox" style="width:110px">
+        To: <input class="easyui-datebox" style="width:110px">
+        Language: 
+        <select class="easyui-combobox" panelHeight="auto" style="width:100px">
+            <option value="java">Java</option>
+            <option value="c">C</option>
+            <option value="basic">Basic</option>
+            <option value="perl">Perl</option>
+            <option value="python">Python</option>
+        </select>
+        <a href="#" class="easyui-linkbutton" iconCls="icon-search">Search</a>
+    </div>
+    <div id="ft" style="padding:2px 5px;">
+        <a href="#" class="easyui-linkbutton" iconCls="icon-add" plain="true"></a>
+        <a href="#" class="easyui-linkbutton" iconCls="icon-edit" plain="true"></a>
+        <a href="#" class="easyui-linkbutton" iconCls="icon-save" plain="true"></a>
+        <a href="#" class="easyui-linkbutton" iconCls="icon-cut" plain="true"></a>
+        <a href="#" class="easyui-linkbutton" iconCls="icon-remove" plain="true"></a>
+    </div>
+		
+		</div><!--endcenter-->
+    </div>
+    <script type="text/javascript">
+        function addPanel(){
+            var region = $('#region').val();
+            var options = {
+                region: region
+            };
+            if (region=='north' || region=='south'){
+                options.height = 50;
+            } else {
+                options.width = 100;
+                options.split = true;
+                options.title = $('#region option:selected').text();
+            }
+            $('#cc').layout('add', options);
+        }
+        function removePanel(){
+            $('#cc').layout('remove', $('#region').val());
+        }
+    </script>
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>杭州电子科技大学研究所科研项目管理系统登录</title>
+    <link href="css/alogin.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div class="Main">
+        <ul>
+            <li class="top"></li>
+            <li class="top2"></li>
+            <li class="topA"></li>
+            <li class="topB"><span>
+                <img src="images/login/logo.gif" alt="loginlogo" style="" />
+            </span></li>
+            <li class="topC"></li>
+            <li class="topD">
+                <ul class="login">
+                    <li><span class="left">用户名：</span> <span style="left">
+                        <input id="Text1" type="text" class="txt" />  
+                     
+                    </span></li>
+                    <li><span class="left">密 码：</span> <span style="left">
+                       <input id="Text2" type="text" class="txt" />  
+                    </span></li>
+                      
+                    
+                    <li>
+                    <span class="left">记住我：</span>
+                       
+                        <input id="Checkbox1" type="checkbox" />
+                 
+                    
+                    </li>
+                    
+                </ul>
+            </li>
+            <li class="topE"></li>
+            <li class="middle_A"></li>
+            <li class="middle_B"></li>
+            <li class="middle_C">
+            <span class="btn">
+               
+                <img alt="" src="images/login/btnlogin.gif" />
+    
+           
+            </span>
+            </li>
+            <li class="middle_D"></li>
+            <li class="bottom_A"></li>
+            <li class="bottom_B">
+            copyright@2014 杭州电子科技大学
+            </li>
+        </ul>
+    </div>
+    </form>
+</body>
+</html>
