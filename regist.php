1d035b8c7ead8e4b537b99aa7809b8be0f214e72
zs1314zt <994249762@qq.com>
2014-12-04 19:51:10 +0800

    创建注册界面

---
+++ regist.php
@@ -0,0 +1,20 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>注册</title>
+    </head>
+    <body>
+        <div class="regist_header"><img src="images/index/logo.png" alt="logo"/>
+            <hr style="FILTER: alpha(opacity=100,finishopacity=0,style=3)" width="90%" color=#4572a5 SIZE=2>
+        </div>
+        <div class="regist_content">
+        </div>
+        
+        
+    </body>
+</html>
