cadd3c292ed14dead98b7d849015d9d18b22a20c
zs1314zt <994249762@qq.com>
2014-12-05 13:26:15 +0800

    regist

--- regist.php
+++ regist.php
@@ -3,18 +3,68 @@
 ?>
 
 <!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8" />
-        <title>注册</title>
-    </head>
-    <body>
-        <div class="regist_header"><img src="images/index/logo.png" alt="logo"/>
-            <hr style="FILTER: alpha(opacity=100,finishopacity=0,style=3)" width="90%" color=#4572a5 SIZE=2>
-        </div>
-        <div class="regist_content">
-        </div>
-        
-        
-    </body>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta charset="utf-8"/>
+    <title>科研项目管理系统注册</title>
+    <link href="css/alogin.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div class="Main">
+        <ul>
+            <li class="top"></li>
+            <li class="top2"></li>
+            <li class="topA"></li>
+            <li class="topB"><span>
+                <img src="images/login/logo.gif" alt="loginlogo" style="" />
+            </span></li>
+            <li class="topC"></li>
+            <li class="topD">
+                <ul class="login">
+                    <li><span class="left">学号/工号：</span> <span style="left">
+                        <input id="Text1" type="text" class="txt" />  
+                     
+                    </span></li>
+                    <li><span class="left">密 码：</span> <span style="left">
+                       <input id="Text2" type="text" class="txt" />  
+                    </span></li>
+                     <li><span class="left">重复密码：</span> <span style="left">
+                       <input id="Text3" type="text" class="txt" />  
+                    </span></li>
+                      
+                    
+                    <li>
+                    <span class="left">实验室：</span>
+            <select  class="txt">
+             <option value="网站建设与维护实验室">网站建设与维护实验室</option>
+             <option value="其他">其他</option>
+             </select>
+                 
+                    
+                    </li>
+                    
+                </ul>
+            </li>
+            <li class="topE"></li>
+            <li class="middle_A"></li>
+            <li class="middle_B"></li>
+            <li class="middle_C">
+            <span class="btn">
+               
+                <img alt="" src="images/login/btnlogin.gif" />
+    
+           
+            </span>
+            </li>
+            <li class="middle_D"></li>
+            <li class="bottom_A"></li>
+            <li class="bottom_B">
+            copyright@2014 杭州电子科技大学
+            </li>
+        </ul>
+    </div>
+    </form>
+</body>
 </html>
+
